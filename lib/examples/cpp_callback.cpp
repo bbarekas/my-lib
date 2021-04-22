fb503de408f4a7333c66fd3ec95a324dfba5c668
Bill Barekas <bb@wappier.com>
2021-04-22 09:15:46 +0300

    Add callback with capturing lambda and mutable parameter.

--- lib/examples/cpp_callback.cpp
+++ lib/examples/cpp_callback.cpp
@@ -6,6 +6,7 @@
  **/
 
 #include <iostream>
+#include<string>
 using namespace std;
 
 #include "mylib.h"
@@ -16,6 +17,22 @@ int main() {
     mylib_callback_cpp(5, [](int n){
         std::printf(" [Lambda 1] n = %d \n", n);
     });
+
+    /** Capturing lambdas. */
+    std::string text = "Home";
+    mylib_callback_cpp(5, [=](int n){
+        std::printf(" [Lambda 2] %s = %d \n", text.c_str(), n);
+    });
+    std::printf(" Text: %s\n", text.c_str());
+
+    /** Mutating lambdas. */
+    mylib_callback_cpp(5, [&text](int n){
+        std::printf(" [Lambda 2] %s = %d \n", text.c_str(), n);
+        text = text + std::to_string(n);
+    });
+
+    std::printf(" Text: %s\n", text.c_str());
+
 
     return 0;
 }
